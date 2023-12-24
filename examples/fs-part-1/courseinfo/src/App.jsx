b92d8a7dd739c8de04b9dcb2a872b3296b7ab34b
nyassian <ebrimanyassi42@gmail.com>
2023-12-24 08:53:25 +0000

    Exercise-1.5

--- examples/fs-part-1/courseinfo/src/App.jsx
+++ examples/fs-part-1/courseinfo/src/App.jsx
@@ -51,30 +51,39 @@ const Total = ({parts}) =>{
 
 
 const App = () =>{
-  const course = 'Half Stack application development'
-  const part1 = { 
+  const course ={ 
+  name : 'Half Stack application development',
+
+  parts :[
+
+ 
+  { 
     name : 'Fundamental of react',
     exercises : 10
-  }
-
-  const part2 = {
+  },
+   
+  {
     name : 'Using props to pass data',
     exercises : 7
-  }
-   const part3 ={
+  },
+    
+  {
     name : 'State of a component',
     exercises : 14
   }
+
+    ]
   
-console.log(part1,part2,part3)
+  }
+
   return (
     <div>
-      <Header  title={course}/>
-      <Content part1={part1.name} exercise1={part1.exercises}
-      part2={part2.name} exercise2={part2.exercises} 
-      part3={part3.name} exercise3={part3.exercises}/>
+      <Header  title={course.name}/>
+      <Content part1={course.parts[0].name} exercise1={course.parts[0].exercises}
+      part2={course.parts[1].name} exercise2={course.parts[1].exercises} 
+      part3={course.parts[2].name} exercise3={course.parts[2].exercises}/>
     
-      <Total   parts={part1.exercises + part2.exercises + part3.exercises}/>
+      <Total   parts={course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises}/>
     </div>
   )
 }
