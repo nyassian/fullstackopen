0d601e93d1f593c078f898265065fa99d6996099
nyassian <ebrimanyassi42@gmail.com>
2023-12-27 07:02:51 +0000

    Exercise-1.8

--- examples/fs-part-1/unicafe/src/App.jsx
+++ examples/fs-part-1/unicafe/src/App.jsx
@@ -21,60 +21,42 @@ const Button = ({good,neutral,bad}) =>{
 )
 
 }
-const Statistics = ({title,text,value}) =>{
- 
-  return(
+
+
+const Statistics = (props) =>{
+
+
+ return(
     <div>
-     <h1>{title}</h1>
-      <div >{text} {value}</div>
+     <h1>{props.title}</h1>
+      <div >{props.text} {props.value}</div>
     </div>
   )
 
-}
-
-const Total = ({all}) =>{
-  return(
-    <div>
-     All {all}
-
-    </div>
-  )
-}
-
-const Average = (props) => {
-  return(
-    <div>
-      Average {props.average}
-    </div>
-  )
-}
-
-const Positive = (props) =>{
-  
-  return(
-    <div>
-       positive {props.positive}%
-    </div>
-  )
-}
+ }
 
 
-const App = () =>{
+
+  const App = () =>{
+
+
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
   
-const handleGoodClick = (goodValue) =>{
+  
+  const handleGoodClick = (goodValue) =>{
+
   setGood(goodValue)
 
   }
 
-const handleNeutralClick = (neutralValue) =>{
+ const handleNeutralClick = (neutralValue) =>{
   setNeutral(neutralValue)
 
   }
 
-  const handleBadClick = (badValue) =>{
+ const handleBadClick = (badValue) =>{
     setBad(badValue)
   
     }
@@ -84,19 +66,16 @@ const handleNeutralClick = (neutralValue) =>{
      <FeedBack title='give feedback' />
 
      <Button good={() => handleGoodClick(good  + 1)}
-       neutral={() => handleNeutralClick(neutral + 1)} 
-     bad={() => handleBadClick(bad + 1)}/>
+             neutral={() => handleNeutralClick(neutral + 1)} 
+             bad={() => handleBadClick(bad + 1)} />
 
-     <Statistics title = 'statistics'/>  
-     <Statistics text = 'good' value={good} />
-     <Statistics text = 'neutral' value={neutral} />
-     <Statistics text = 'bad' value={bad} />
-
-     <Total  all={good + neutral + bad} />
-     
-     <Average average={(good + neutral + bad) / 3 }/>
-
-     <Positive positive={((good + 0 - bad) / 3) * 100}/>
+     <Statistics  title = 'statistics' />  
+     <Statistics  text = 'good' value={good} />
+     <Statistics  text = 'neutral' value={neutral} />
+     <Statistics  text = 'bad' value={bad} />
+     <Statistics  text = 'all' value={good + neutral + bad} />
+     <Statistics  text = 'average' value={(good + neutral + bad) / 3 } />
+     <Statistics  text = 'positive' value ={((good + 0 - bad) / 3) * 100}  />
 
     </div>
   )
